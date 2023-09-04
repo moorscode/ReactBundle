103a30f251526788db21e9c52d897be509249b5f
Jip <jhp.moors@gmail.com>
2023-09-05 00:09:09 +0200

    Inject variables directly

--- DependencyInjection/LimeniusReactExtension.php
+++ DependencyInjection/LimeniusReactExtension.php
@@ -27,24 +27,12 @@ class LimeniusReactExtension extends Extension
         $container->setParameter('limenius_react.dom_id_prefix', $config['dom_id_prefix'] || '');
         $container->setParameter('limenius_react.fail_loud', $config['serverside_rendering']['fail_loud']);
         $container->setParameter('limenius_react.trace', $config['serverside_rendering']['trace']);
+        $container->setParameter('limenius_react.server_socket_path', $config['serverside_rendering']['server_socket_path']);
+        $container->setParameter('limenius_react.server_bundle_path', $config['serverside_rendering']['server_bundle_path']);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
         $loader->load('twig.xml');
-
-        $defaultRendering = $config['default_rendering'];
-
-        if ('both' === $defaultRendering || 'server_side' === $defaultRendering) {
-            $rendererFactory = $container->getDefinition('limenius_react.render_factory');
-
-            if ($serverSocketPath = $config['serverside_rendering']['server_socket_path']) {
-                $rendererFactory->addMethodCall('setServerSocketPath', array($serverSocketPath));
-            }
-
-            if ($serverBundlePath = $config['serverside_rendering']['server_bundle_path']) {
-                $rendererFactory->addMethodCall('setServerBundlePath', array($serverBundlePath));
-            }
-        }
 
         $cache = $config['serverside_rendering']['cache'];
         $container->setParameter('limenius_react.cache_enabled', $cache['enabled']);
